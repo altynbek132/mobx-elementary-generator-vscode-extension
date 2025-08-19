d99cf85f6691a91b9bd9837f9088515933acae0e
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2025-08-19 15:25:21 +0500

    refactor(template): change Body from StatelessWidget to StatefulWidget

--- resources/template.dart
+++ resources/template.dart
@@ -26,9 +26,14 @@ extension BuildContextTemplateWmExtension on BuildContext {
   TemplateWm get readTemplateWm => read<TemplateWm>();
 }
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   const Body({super.key});
 
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     return Observer(
